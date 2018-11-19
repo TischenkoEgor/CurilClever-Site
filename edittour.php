b556c712c4ce1b20a0fa4f43e04de409b53c0a53
TischenkoEgor <TischenkoEgor@gmail.com>
2018-11-19 21:57:29 +0300

    редактирование, создание тура, редирект после операций

--- edittour.php
+++ edittour.php
@@ -17,6 +17,8 @@
         // если  
 
         $createMode = 0;
+        if(isset($_GET['create']))
+            $createMode = 1;
 
         $tour_id = -1;
         $tourname = "";
@@ -33,7 +35,7 @@
         if(isset($_GET["tourid"]))
         {
              
-            $tour_id = $_GET["orderid"];
+            $tour_id = $_GET["tourid"];
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             //1. подключаемся к серверу
             $link = mysqli_connect($host, $user, $password, $database) 
@@ -45,6 +47,8 @@
                 tours.id AS id, 
                 clients.first_name AS person_first_name, 
                 clients.second_name AS person_second_name, 
+                clients.id AS clientid,
+                tours.name AS name,
                 hotels.name AS hotel_name, 
                 tours.begin_date AS begin_date, 
                 tours.end_date AS end_date, 
@@ -56,7 +60,7 @@
             INNER JOIN clients ON client_id = clients.id
             INNER JOIN hotels ON hotel_id = hotels.id
                     WHERE 
-                        orders.orderid=".$order_id; 
+                        tours.id=".$tour_id; 
 
 
             $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));     
@@ -95,7 +99,16 @@
 
         $error_num = 0;
         $input_num = 0;
-
+        
+        if(isset($_POST['tourname'])){
+            $input_num++;
+            $tourname = $_POST['tourname'];
+            if(strlen($tourname) == 0)
+            {
+                $clientid_mes = "название не может быть пустым";
+                $error_num ++;
+            }
+        }
        
         if(isset($_POST['clientid'])){
             $input_num++;
@@ -163,9 +176,10 @@
             $comment = $_POST['comment'];
         }
 
+
+        // создание нового тура в базе
         if(isset($_POST['create_tour']) && $error_num == 0)
         {
-            echo_warning_msg("попытка сохранения: сохранение в базу еще не работает");
              //1. подключаемся к серверу
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             $link = mysqli_connect($host, $user, $password, $database) 
@@ -190,8 +204,8 @@
             $stmt = mysqli_prepare($link, $query);  
 
             //4. вставим данные из формы 
-            $stmt->bind_param("ssssssii",
-                $OrderName, 
+            $stmt->bind_param("siisssii",
+                $tourname, 
                 $clientid, 
                 $hotelid, 
                 date("Y-m-d H:i:s", strtotime($date_arrival)), 
@@ -202,28 +216,93 @@
         
             $stmt->execute();
             echo_positive_msg("успеш6но создано, брат!");
-            // 2. Тело запроса на удаление заявки после создания тура
-            $query = 
-            "DELETE FROM
-                orders 
-            WHERE 
-                orderid=?";
 
+
+            $tour_id = mysqli_insert_id($link);
+            $js_redir = "<script type='text/javascript'>  
+                            setTimeout(function () 
+                            { 
+                                window.location.href = 'edittour.php?tourid=".$tour_id."'; 
+                            }, 2000);  
+                        </script>";    
+
+            echo $js_redir;    
+            exit();                    
+        }
+
+        // сохранение изменений параметров тура
+        if(isset($_POST['save_tour']) && $error_num == 0)
+        {
+            //1. подключаемся к серверу
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+            $link = mysqli_connect($host, $user, $password, $database) 
+            or die("Ошибка " . mysqli_error($link));
+            //2. создадим  запрос
+
+            $query = 
+            "UPDATE  
+                    tours 
+                SET 
+                    name=?,
+                    client_id=?,
+                    hotel_id=?,
+                    begin_date=?,
+                    end_date=?,
+                    comment=?,
+                    price=?,
+                    pay_status=?
+            WHERE
+                id=?";
+                 
+            //3. подготовим запрос       
+            $stmt = mysqli_prepare($link, $query);  
+           
+            //4. вставим данные из формы 
+            $stmt->bind_param("siisssiii",
+                $tourname, 
+                $clientid, 
+                $hotelid, 
+                date("Y-m-d H:i:s", strtotime($date_arrival)), 
+                date("Y-m-d H:i:s", strtotime($date_departure)),
+                $comment,
+                $price,
+                $pay_status,
+                $tour_id);
+         
+            $stmt->execute();
+            echo_positive_msg("тур успешно обновлен, бро!");
+            print_r($_POST);
+        }
+
+        // удалеие тура из базы
+        if(isset($_POST['delete_tour']))
+        {
+             //1. подключаемся к серверу
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+            $link = mysqli_connect($host, $user, $password, $database) 
+            or die("Ошибка " . mysqli_error($link));
+            //2. создадим  запрос
+
+            $query = 
+            "DELETE 
+                FROM 
+                    tours
+                WHERE
+                    id=?";
+                
             //3. подготовим запрос       
             $stmt = mysqli_prepare($link, $query);  
 
-            //4. вставим нмоер заявки
-            $stmt->bind_param("i", $order_id);
-
+            //4. вставим данные из формы 
+            $stmt->bind_param("i",$tour_id);
+        
             $stmt->execute();
-            echo_positive_msg("успешно удалена заявка, блеат!");
-           
-            
+            echo_positive_msg("тур успешно удален!");
 
             $js_redir = "<script type='text/javascript'>  
                             setTimeout(function () 
                             { 
-                                window.location.href = 'orders.php.'; 
+                                window.location.href = 'tours.php'; 
                             }, 2000);  
                         </script>";    
 
@@ -233,7 +312,35 @@
 
         ?>
         <div class="page">
-            <H2>Создание тура из заявки</H2>
+
+            <?
+                if(!$createMode)
+                {
+                    ?>
+                        <h2>Управление туром</h2>
+                        <fieldset>
+                            <legend>Внимание, операции необратимы!</legend>
+                            <form action="edittour.php?<?if($tour_id > 0)echo 'tourid='.$tour_id;?><?if($createMode > 0)echo '&create=1';?>" method="post" name="controltourform">
+                                <p><input type="submit" name='_tour' value="еще кнопка" disabled></p>
+                                                               
+                                <p><input type="submit" name='delete_tour' value="удалить тур"></p>
+                            </form>
+
+
+                        </fieldset>
+                    <?
+                }
+            ?>
+
+            <H2>
+            <?
+            if($createMode)
+                echo 'Создание тура';
+            else
+                echo 'Изменение тура';
+            ?>
+            
+            </H2>
             <fieldset>
                 <legend>Данные тура</legend>
                 <?
@@ -242,30 +349,48 @@
                     echo_input_error("Не удалось созлать тур: некорректные параметры!");    
                 } 
                 ?>
-                <form action="createtour.php?orderid=<?echo $order_id?>" method="post" name="configuretourform">
+                <form action="edittour.php?<?if($tour_id > 0)echo 'tourid='.$tour_id;?><?if($createMode > 0)echo '&create=1';?>" method="post" name="configuretourform">
 
-                    <p><label> Название заявки: <input name="OrderName" type="text" value="<?echo $OrderName;?>" disabled></label></p>
+
+                    <?
+                        //вывод скрытых полей - дублеров для disabled полей
+
+                        if(!$createMode)
+                        {
+                            ?>
+                                <input name="tourname" type="hidden" value="<?echo $tourname;?>">
+                                <input name="clientid" type="hidden" value="<?echo $clientid;?>">
+                            <?
+                        }
+                        
+                    
+                    ?>
+
+
+                    <p><label> Название тура: <input name="<?if($createMode) echo 'tourname'?>" type="text" value="<?echo $tourname;?>" <?if(!$createMode) echo 'disabled';?>></label></p>
                     
                     <? echo_input_error($clientid_mes); ?>
-                    <p><label> Клиент:  <select name="clientid" >
+                    <p><label> Клиент:  <select name="<?if($createMode) echo 'clientid'?>" <?if(!$createMode) echo 'disabled';?>>
                         <?
                         // подключаемся к серверу
                         $link = mysqli_connect($host, $user, $password, $database) 
                         or die("Ошибка " . mysqli_error($link));
 
-                        // заюираем всех клиентов из базы данных
+                        // забираем всех клиентов из базы данных
                         $query ="SELECT * FROM clients";
                         $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));  
                         while($person = mysqli_fetch_array($result))
                         {
                             ?>
-                            <option  value="<?echo $person['id'];?>"  <?if($person['id'] == $clientid) echo "selected";?> ><?echo $person['first_name'].' '.$person['second_name'];?></option>
+                            <option  value="<?echo $person['id'];?>" <?if($person['id'] == $clientid) echo "selected";?> ><?echo $person['first_name'].' '.$person['second_name'];?></option>
                             <? 
                         }
                         ?>
                     </select></label></p>
+
+                   
                     
-                    <p><label> Отель:  <select name="hotelid" disabled>
+                    <p><label> Отель:  <select name="hotelid">
                         <?
                         // подключаемся к серверу
                         $link = mysqli_connect($host, $user, $password, $database) 
@@ -284,14 +409,11 @@
                         </select>
                     </label></p>
                     
-
-
-
                     <p><?echo_input_error($begin_date)?></p>
-                    <p><label> Заселение: <input type="date" name="date_arrival" value="<?echo $date_arrival;?>" disabled></label></p>
+                    <p><label> Заселение: <input type="date" name="date_arrival" value="<?echo $date_arrival;?>"></label></p>
                     
                     <p><?echo_input_error($end_date)?></p>
-                    <p><label> Выезд: <input type="date" name="date_departure" value="<?echo $date_departure;?>" disabled></label></p>
+                    <p><label> Выезд: <input type="date" name="date_departure" value="<?echo $date_departure;?>"></label></p>
                     
                     <p><?echo_input_error($price_mes)?></p>
                     <p><label> Полная стоимость: <input type="number" name="price" value="<?echo $price;?>"> руб.</label></p>
@@ -306,9 +428,22 @@
                     </label></p>                    
 
                     <p><label>Комментарий к туру</label></p>
-                    <p><textarea name="comment" value="<?echo $comment;?>" rows="7" cols="46"> </textarea></p>      
-
-                    <p><input name="create_tour" type="submit" value="Зарегистрировать тур"></p>
+                    <p><textarea name="comment" rows="7" cols="46"><?echo $comment;?> </textarea></p>      
+                    
+                    <?
+                    if($createMode)
+                    {
+                    ?>
+                        <p><input name="create_tour" type="submit"  value="создать тур"></p>
+                    <?       
+                    }
+                    else
+                    {
+                    ?>
+                        <p><input name="save_tour" type="submit"  value="сохранить тур"></p>
+                    <?
+                    }
+                    ?>
                 </form>
             </fieldset>
         </div>
