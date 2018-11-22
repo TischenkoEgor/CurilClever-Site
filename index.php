ce53e533d3d069c4d8229482518fcd3606906572
dev one <mrmihap@yandex.ru>
2018-11-22 20:58:39 +0300

    обновление первой страницы
    изменение размеров блоков
    + граничение по количеству отелей
    + ссылка на все отели
    + вместо людей заявки
    + ограничение заявок
    + ссылка на все заявки

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 				<h2>Главная страница: управление турами</h2>
 			</div>
 			<div id="preview1stline">
-				<div class="preview" id="orders">
+				<div class="preview" id="LastOrders">
 					<h3>Последние заявки</h3>
 					<?
 					// подключаемся к серверу
@@ -23,7 +23,19 @@
 					or die("Ошибка " . mysqli_error($link));
 
 					// выполняем операции с базой данных
-					$query ="SELECT * FROM clients";
+					$query ="SELECT
+						orders.orderid AS order_id,
+						clients.first_name AS person_first_name, 
+						clients.second_name AS person_second_name, 
+						hotels.name AS hotel_name,
+						order_registration_date as reg_date
+						FROM orders
+							INNER JOIN clients 
+								ON person_id = clients.id
+							INNER JOIN hotels 
+								ON hotel_id = hotels.id
+						ORDER BY  orders.order_registration_date DESC
+						LIMIT 6";
 					$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
 					if($result)
 					{
@@ -33,13 +45,12 @@
 							?>
 							<div > <p class="ordershortview">
 							<?
-								echo $row['first_name'].' '.$row['second_name'].' '.$row['email']; 
+								echo 'в '.$row['hotel_name'].' от '.$row['reg_date']; 
 							?>
+							<a href="controlorder.php?id=<? echo $row['order_id'] ?>">просм</a>
 							</p></div>
 							<?
 						}
-						
-						
 						// очищаем результат
 						mysqli_free_result($result);
 						
@@ -47,11 +58,13 @@
 					// закрываем подключение
 					mysqli_close($link); 
 					?>
+					<hr>
+					<a href="allorders.php">просмотр всех</a>
 				</div>
-				<div class="preview" id="tours">
+				<div class="preview" id="LastTours">
 					<h3>Последние туры</h3>	
 				</div>
-				<div class="preview" id="hotels">
+				<div class="preview" id="TopHotels">
 					<h3>Популярные отели</h3>	
 					
 					<?
@@ -60,7 +73,7 @@
 					or die("Ошибка " . mysqli_error($link));
 
 					// выполняем операции с базой данных
-					$query ="SELECT * FROM hotels";
+					$query ="SELECT * FROM hotels LIMIT 14";
 					$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
 					if($result)
 					{
@@ -70,7 +83,7 @@
 							?>
 							<div> 
 							<?
-								echo $row['name'].' '.$row['price'].' '.$row['stras_rate'].'*' ; 
+								echo $row['stars_rate'].'* '.$row['name'].' '.$row['price'].'$' ; 
 							?>
 							</div>
 							<?
@@ -84,6 +97,8 @@
 					// закрываем подключение
 					mysqli_close($link); 
 					?>
+					<hr>
+					<a href="hotels.php"> просмотр всех </a>
 				</div>
 				<div id="touristtraveltoday">
 					<h3>Туристы в путешествии на сегодня</h3>
